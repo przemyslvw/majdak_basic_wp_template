bcf13efb91147d09b3cfedf61774bff636279903
przemyslvw <przemyslvw@gmial.com>
2024-05-20 19:33:24 +0200

    echo get_bloginfo

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>majdak.online | Web Application Development | Programowanie Aplikacji Internetowych</title>
+    <title><?php echo get_bloginfo('name'); ?></title>
     <meta name="description"
         content="Specializing in web application development and programming. Specjalizujemy się w programowaniu i tworzeniu aplikacji internetowych.">
     <meta name="keywords"
